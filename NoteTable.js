97aa1747693b580bd045ab0aeb792b2438c4a4ea
Laramie Crocker <git@laramiecrocker.com>
2026-02-26 16:41:13 -0800

    Added as Facade around notetable.js

---
+++ NoteTable.js
@@ -0,0 +1,38 @@
+
+
+import {
+	colorNote,
+	buildCellsFromSelector,
+	replay,
+	showHighlightsForBeat,
+	fullRepaint,
+	clearAll
+} from './notetable.js';
+
+class NoteTable {
+	static colorNote(cell) {
+		return colorNote(cell);
+	}
+
+	static buildCellsFromSelector(selector, noteLetter, sharpflat, noteNum, options) {
+		return buildCellsFromSelector(selector, noteLetter, sharpflat, noteNum, options);
+	}
+
+	static replay() {
+		return replay();
+	}
+
+	static showHighlightsForBeat(nBeat) {
+		return showHighlightsForBeat(nBeat);
+	}
+
+	static fullRepaint() {
+		return fullRepaint();
+	}
+
+	static clearAll() {
+		return clearAll();
+	}
+}
+
+export { NoteTable };
