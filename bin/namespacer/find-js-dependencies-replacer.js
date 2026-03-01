b3cf92c53ce4c69fb0c72ed842068af761e98cbd
Laramie Crocker <git@laramiecrocker.com>
2026-03-01 12:21:05 -0800

    Files ready for prototyping

--- bin/namespacer/find-js-dependencies-replacer.js
+++ bin/namespacer/find-js-dependencies-replacer.js
@@ -1,3 +1,12 @@
+#!/usr/bin/env node
+/* Node.js utility to search files in a directory
+    Run on the bash command line like so, since ths file has a shebang.
+        cd ~/infinite-neck/bin/namespacer
+        ./find-js-dependencies-replacer.js
+    e.g.
+        laramie@penguin:~/infinite-neck$ ./bin/find-js-dependencies.js --h
+*/
+
 class Line {
     constructor({ identifier, startIndex, linenum, rawLine, replacedLine = '', namespace = '', regexUsed = null }) {
         this.identifier = identifier;      // The bare identifier matched
@@ -10,20 +19,44 @@ class Line {
     }
 }
 
-const IDENTIFIERS = ["invocationOne", "invocationTwo", "invocationThree"];
-const FILEPATH = "./namespacer/data/song.js";
+const PLANS_DIRPATH =  "./data/plans";
+const TMP_DIRPATH =    "./data/tmp";
+const OUT_DIRPATH =    "./data/out/";
+
+const SRC_FILEPATH =   "./data/src/song.js";
+const OUTPUT_FILEPATH =   "./data/out/generated-song.js";
+
+// ====== Already kinda handled =======
+// song.js            :: [trigger]                [EventBus]            (song.js imports EventBus explicity so can modularly call EventBus.trigger()  == .trigger() should not be scanned because it is already invoke on an object/class/namespace with a dot. 
+//
+// ====== Good examples for testing ==========
+// NoteTableFacade.js :: [clearAll]               (NoteTableFacade]  ( IS_A Namespace)
+// NoteTableFacade.js :: [replay]                 [InfiniteNeckFacade]  (replay() is already exported from infinite-neck.js)
+// infinite-neck.js   :: [clearAndReplaySection]  [InfiniteNeckFacade]  (infinite-neck.js is not a Namespace, but should become one or be wrapped by a facade which is a Namespace.
+// infinite-neck.js   :: [resetNoteNames]         [InfiniteNeckFacade]  (already exported from infinite-neck.js)
+
+const IDENTIFIERS = ["clearAll", "clearAndReplaySection", "replay", "resetNoteNames"];
+
+const namespaceMap = {
+    clearAll:               'NoteTableFacade',
+    replay:                 'InfiniteNeckFacade',
+    clearAndReplaySection:  'InfiniteNeckFacade',
+    resetNoteNames:         'InfiniteNeckFacade'
+};
+
+
+function main(){
+    let content = readSourceFile(SRC_FILEPATH);
+    let inMemStruct = createLinesDataStructure(content, IDENTIFIERS);
+    console.log("output data struct:\n"+JSON.stringify(inMemStruct,null,4));
+}
 
 function readSourceFile(filePath){
     return fs.readFileSync(filePath, 'utf8');
-
 }
 
-function main(){
-    let content = readSourceFile(FILEPATH);
-    createLinesDataStructure(IDENTIFIERS, content);
-}
 
-function createLinesDataStructure(identifiers, content){
+function createLinesDataStructure(content, identifiers){
     const linesArr = content.split('\n');
 
     const identifierPattern = identifiers.join('|');
@@ -47,14 +80,9 @@ function createLinesDataStructure(identifiers, content){
             regexUsed: lineRegex
         }));
     }
+    return lineObjects;
 }
 
-const namespaceMap = {
-    invocationOne: 'newNamespaceName1',
-    invocationTwo: 'newNamespaceName2',
-    invocationThree: 'newNamespaceName3'
-    // ...etc
-};
 
 function generateReplacedLines(lineObjects){
     lineObjects.forEach(lineObj => {
@@ -79,6 +107,9 @@ function writeOutReplacedLines(){
         linesArr[lineObj.linenum - 1] = lineObj.replacedLine;
     });
     const newContent = linesArr.join('\n');
-    fs.writeFileSync(outputFilePath, newContent, 'utf8');
+    fs.writeFileSync(OUTPUT_FILEPATH, newContent, 'utf8');
 }
+
+//==========  Do it! ================
+main();
 
